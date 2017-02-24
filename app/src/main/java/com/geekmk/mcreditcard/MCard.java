2c10d9a2e026fa66f98992b1453cc092ce61f09c
Manikanta <manikanta.garikipati@gmail.com>
2017-02-24 11:40:55 +0530

    CardLayout design.

--- app/src/main/java/com/geekmk/mcreditcard/MCard.java
+++ app/src/main/java/com/geekmk/mcreditcard/MCard.java
@@ -1,32 +1,39 @@
 package com.geekmk.mcreditcard;
 
 import android.content.Context;
-import android.support.v7.widget.CardView;
 import android.util.AttributeSet;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.RelativeLayout;
 
 /**
  * Created by Mani on 23/02/17.
  * Custom card that contains its design of a conventional credit/debit card
  */
 
-public class MCard extends CardView{
+public class MCard extends RelativeLayout{
 
     public MCard(Context context) {
         super(context);
-        initViews();
+        initViews(context);
     }
 
     public MCard(Context context, AttributeSet attrs) {
         super(context, attrs);
-        initViews();
+        initViews(context);
     }
 
     public MCard(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
-        initViews();
+        initViews(context);
     }
 
-    private void initViews() {
+    private void initViews(Context context) {
+
+        LayoutInflater inflater = (LayoutInflater) context
+                .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+        View view = inflater.inflate(R.layout.item_mcard, this);
 
     }
 }
